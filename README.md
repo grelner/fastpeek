08504773eca607d83db1b3484ed1b15e260951e5
Christian Schjølberg <cschjolb@gmail.com>
2024-09-26 16:45:21 +0200

    Blanket implementation for AsRef<T>

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# fastpeek
+# fastpeek [![rust-version-badge][]][rust-version]
 
 This crate aims to provide a way to peek iterators without the overhead of
-[Peekable]. It does so by providing the trait [PeekableIter] which are
-implemented directly on existing iterators. It does not intercept and buffer
-output from [Iterator::next] like [Peekable] and thus there is no overhead
-when doing normal iteration.
+Peekable on normal iteration. It does so by providing the trait PeekableIter
+which are implemented directly on iterators that provide access to the
+underlying data. This allows us to do normal iteration without any overhead,
+as peek() does not have to call next() and buffer the result.
+
+A blanket implementations is provided for Iterators that implement AsRef<[T]>.
+
+[rust-version-badge]: https://img.shields.io/badge/rust-latest%20stable-blue.svg?style=flat-square
+
+[rust-version]: https://github.com/grelner/fastpeek#rust-version-policy
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,52 @@
-pub trait PeekableIter: Iterator {
-    fn peek(&self) -> Option<Self::Item>;
+struct Owned;
+struct Borrowed;
+
+/// A trait that can be implemented for iterators that allow accessing the underlying data
+/// without calling next(). There are blanket implementations for all iterators that implement
+/// AsRef<[Self::Item]>
+pub trait PeekableIter<'a, T>: Iterator {
+    type PeekItem;
+    fn peek(&'a self) -> Option<Self::PeekItem>;
+}
+
+impl<'a, T, I> PeekableIter<'a, Owned> for I
+where
+    T: 'a,
+    I: Iterator<Item = T> + AsRef<[T]>,
+{
+    type PeekItem = &'a T;
+
+    fn peek(&'a self) -> Option<Self::PeekItem> {
+        self.as_ref().first()
+    }
+}
+
+impl<'a, T, I> PeekableIter<'a, Borrowed> for I
+where
+    T: 'a,
+    I: Iterator<Item = &'a T> + AsRef<[T]>,
+{
+    type PeekItem = &'a T;
+
+    fn peek(&'a self) -> Option<Self::PeekItem> {
+        self.as_ref().first()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::PeekableIter;
+    #[test]
+    fn test_vec_into_iter() {
+        let vec = vec![1, 2, 3];
+        let mut i = vec.into_iter();
+        assert_eq!(i.peek().cloned(), i.next());
+    }
+
+    #[test]
+    fn test_vec_iter() {
+        let vec = vec![1, 2, 3];
+        let mut i = vec.iter();
+        assert_eq!(i.peek().cloned(), i.next().cloned());
+    }
 }
